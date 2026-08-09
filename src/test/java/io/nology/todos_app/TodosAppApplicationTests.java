6e523fab3b48c1ddf992e555b595410375bcedb6
LewisYoon <trexhoon1212@gmail.com>
2026-08-09 16:42:06 +1000

    adds basic spring boot folder structures with maven dependencies

---
+++ src/test/java/io/nology/todos_app/TodosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nology.todos_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
